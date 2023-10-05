87e8e54e988817584e649f0cf3bb1000f926f738
SamundraNeupane <wdymsamundra@gmail.com>
2023-10-05 23:02:52 +0545

    testingback

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
@@ -19,21 +21,57 @@ final FlutterLocalNotificationsPlugin flutterLocalPlugin = FlutterLocalNotificat
 const AndroidNotificationChannel notificationChannel = AndroidNotificationChannel("Testing Notification", "NotiTester",
 description: "Something Descriptive", importance: Importance.high);
 
-// Future <void> initservice()async{
-//   await flutterLocalPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(notificationChannel);
-//   var service = FlutterBackgroundService();
-//     await service.configure(
-//       iosConfiguration: IosConfiguration(
-//           onBackground: iosBackground,
-//           onForeground: onStart
-//       ),
-//       androidConfiguration: AndroidConfiguration(
-//         onStart: onStart, 
-//         autoStart: true,
-//         isForegroundMode: isForegroundMode
-//         )
-//     );
-// }
+Future <void> initservice()async{
+  
+  var service = FlutterBackgroundService();
+  if(Platform.isIOS){
+    await flutterLocalPlugin.initialize(const InitializationSettings(
+      iOS: DarwinInitializationSettings()
+    ));
+  }
+  await flutterLocalPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(notificationChannel);
+  
+  await service.configure(
+      iosConfiguration: IosConfiguration(
+          onBackground: iosBackground,
+          onForeground: onStart
+      ),
+      androidConfiguration: AndroidConfiguration(
+        onStart: onStart, 
+        autoStart: true,
+        isForegroundMode: true,
+        notificationChannelId: "HelloKavach",
+        initialNotificationContent: "Running Kavach",
+        foregroundServiceNotificationId: 90,
+        )
+    );
+    service.startService();
+}
+
+@pragma("vm:enry-point")
+void onStart (ServiceInstance service){
+  DartPluginRegistrant.ensureInitialized();
+  service.on("setAsForeground").listen((event) { 
+    print("foreground");
+  });
+  service.on("setAsBackground").listen((event) { 
+    print("background");
+  });
+
+
+  Timer.periodic(const Duration (seconds: 2), (timer) {
+    flutterLocalPlugin.show(90, "Notification run", "We are running", const NotificationDetails(android: AndroidNotificationDetails("HelloKavach", " Kavach Noti",ongoing: true, icon: "app_icon")));
+   });
+}
+
+
+@pragma("vm:enry-point")
+Future<bool> iosBackground (ServiceInstance service)async{
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  return true;
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
