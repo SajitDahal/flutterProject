0c1120bb5b90d89840a8a0b3695f331a2bafcfc9
ishan <ishadhh8git config --global user.email ishadhh8@gmail.com>
2023-10-06 12:19:05 +0545

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,6 @@ void main()async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-  final _fpS = get ();
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
